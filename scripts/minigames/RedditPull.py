3b1cad250088f45092d0ca8b7d84713451c502a1
Michael DAgostino <dagostinomichael00@gmail.com>
2022-10-27 12:34:27 -0400

    RedditPull now successfully pulls posts from reddit, using bot account information in the .env file

--- scripts/minigames/RedditPull.py
+++ scripts/minigames/RedditPull.py
@@ -1,12 +1,28 @@
 import praw
-import random
+import os
 import discord
+from requests import Session
+from dotenv import load_dotenv
+
+session = Session()
+session.verify = "/path/to/certfile.pem"
+
+load_dotenv()
+CLIENT_ID = os.getenv('REDDITCLIENTID')
+CLIENT_SECRET = os.getenv('REDDITCLIENTSECRET')
+USER_AGENT = os.getenv('REDDITUSERAGENT')
 
 reddit = praw.Reddit(
-    client_id="my client id",
-    client_secret="my client secret",
-    user_agent="my user agent",
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    user_agent=USER_AGENT,
 )
 
 async def pullRedditPost(ctx):
+    subredditname = "okaybuddyretard"
+    subreddit = reddit.subreddit(subredditname)
+
     await ctx.send(f"Reddit is installed and read only? {reddit.read_only}")
+    for submission in subreddit.hot(limit=1):
+        await ctx.send(f"Post from r/{subredditname} {submission.url}")
+    
