53ee6e58ab993dbb55c31a63219d388fce0ca3f7
Elijah Nieves <elijahnieves328@gmail.com>
2022-11-25 20:28:54 -0500

    Added functions to update the amount of gold or points a user has. Also added test commands to add gold or points.

--- scripts/dbmanagement/SQLiteDBHandler.py
+++ scripts/dbmanagement/SQLiteDBHandler.py
@@ -7,6 +7,7 @@ import dotenv
 import os
 import pandas as pd
 import sqlite3
+from itertools import chain
 
 DBPASSWORD = os.getenv('DBPASSWORD')
 
@@ -19,8 +20,6 @@ def connectDB():
     cur = connection.cursor()
     print("Connection made")
     return connection, cur
-
-
 
 # for creating tables and inserting data
 def execute_query(query):
@@ -44,6 +43,70 @@ def read_query(query):
     except Exception as err:
         print(f"Error: '{err}'")
 
+def update_points(ctx, points_to_add):
+    points_to_add = int(points_to_add)
+    user = str(ctx.message.author)
+    print(user)
+    findUsers = """
+    SELECT User FROM UserStats;
+    """
+    knownUsers = read_query(findUsers)      # returns as nested list
+    knownUsers = chain.from_iterable(knownUsers)
+    print(knownUsers, user)
+    if user in knownUsers:
+        findPoints = f"""
+        SELECT Points FROM UserStats
+        WHERE User = '{user}';
+        """
+        currentPoints = read_query(findPoints)
+        # grab value from nested list
+        currentPoints = currentPoints[0][0]
+
+        newPoints = currentPoints + points_to_add
+        updateQuery = f"""
+        UPDATE UserStats
+        SET Points = {newPoints}
+        WHERE User = '{user}';
+        """
+    else:
+        updateQuery = f"""
+        INSERT INTO UserStats VALUES
+        ('{user}', {points_to_add}, 0);
+        """
+    execute_query(updateQuery)
+
+def update_gold(ctx, gold_to_add):
+    gold_to_add = int(gold_to_add)
+    user = str(ctx.message.author)
+    print(user)
+    findUsers = """
+    SELECT User FROM UserStats;
+    """
+    knownUsers = read_query(findUsers)      # returns as nested list
+    knownUsers = chain.from_iterable(knownUsers)
+    print(knownUsers, user)
+    if user in knownUsers:
+        findGold = f"""
+        SELECT Gold FROM UserStats
+        WHERE User = '{user}';
+        """
+        currentGold = read_query(findGold)
+        # grab value from nested list
+        currentGold = currentGold[0][0]
+
+        newGold = currentGold + gold_to_add
+        updateQuery = f"""
+        UPDATE UserStats
+        SET Gold = {newGold}
+        WHERE User = '{user}';
+        """
+    else:
+        updateQuery = f"""
+        INSERT INTO UserStats VALUES
+        ('{user}', 0, {gold_to_add});
+        """
+    execute_query(updateQuery)
+
 def make_test_table():
     create_test_table = """
     CREATE TABLE Test (
@@ -63,22 +126,19 @@ def make_test_table():
     execute_query(create_test_table) # execute our defined query
     execute_query(pop_test_table)
 
-
-async def view_test_table(ctx):
+async def view_stats(ctx):
     select_query = """
-    SELECT * FROM Test;
+    SELECT * FROM UserStats;
     """
     
     results = read_query(select_query)
 
     for result in results:
         name, points, gold = result
-        await ctx.send("{0}\nPoints: {1}\nGold: {2}".format(name, points, gold))
+        await ctx.send(f"{name}\nPoints: {points}\nGold: {gold}")
 
-async def testdb(ctx):
-    try:
-        make_test_table()
-        print("test table created")
-        await view_test_table(ctx)
-    except Exception as err:
-        print(f"Error: '{err}'")
+async def test_points(ctx):
+    update_points(ctx, int(1))
+
+async def test_gold(ctx):
+    update_gold(ctx, int(1))
