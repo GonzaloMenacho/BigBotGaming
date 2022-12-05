5782b2b669506be038b86ee4f86dab2fd54c6657
Michael DAgostino <dagostinomichael00@gmail.com>
2022-12-04 22:21:35 -0500

    Added security checks to make sure the user playing the game is the user who can enter commands.

--- scripts/minigames/RPGGame/RPGGame.py
+++ scripts/minigames/RPGGame/RPGGame.py
@@ -6,9 +6,13 @@ import time
 import discord.ext.commands.context as ctxt
 import discord
 import re
+import asyncio
+
+current_users = set()
 
 async def playRPG(ctx : ctxt):
-    await set_up_game_channel(ctx)
+    thread = await set_up_game_channel(ctx)
+    await wait_for_message_in_channel(ctx, thread)
 
 
 async def set_up_game_channel(ctx: ctxt):
@@ -17,6 +21,7 @@ async def set_up_game_channel(ctx: ctxt):
     discordthread = await create_discord_thread(ctx, channelname)
     message = f"Hello {discorduser.name}"
     await send_message_in_thread(discordthread, message)
+    return discordthread
 
 
 def get_thread_name(ctx: ctxt) -> str:
@@ -38,8 +43,8 @@ async def check_if_channel_exists(ctx : ctxt, name):
             req_channel = thread
             #print(thread.name)
 
-    print(req_channel)
-    print(req_channel.id)
+    #print(req_channel)
+    #print(req_channel.id)
     return req_channel
 
 
@@ -57,12 +62,24 @@ async def create_discord_thread(ctx, channelname) -> discord.TextChannel:
     return channel
 
 
-async def send_message_in_channel_with_id(ctx, id):
-    pass
+async def wait_for_message_in_channel(ctx, thread : discord.TextChannel):
+    def check(m):
+        return m.content and m.channel == thread and m.author == ctx.message.author
+
+    try:
+        await thread.send('foo')
+        message = await ctx.bot.wait_for("message", timeout = 15.0, check=check)
+        await thread.send(message.content)
+    except asyncio.TimeoutError:
+        await thread.send("Your time is up")
 
 
 async def send_message_in_thread(channel : discord.TextChannel, message):
     await channel.send(message)
+
+
+def add_user_to_playing_list(ctx):
+    pass
 
 
 async def initialize_character():
